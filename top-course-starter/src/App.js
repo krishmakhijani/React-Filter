db55ef408c2106168e0839e2ff97c079b4e8d165
Krish Makhijani <112251957+velocitypanther@users.noreply.github.com>
2024-02-18 01:08:57 +0530

    Added Filter Dat to App.js

--- top-course-starter/src/App.js
+++ top-course-starter/src/App.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import {apiUrl,filterData} from "./data"
+import { toast } from "react-toastify";
+import { useState } from "react";
+
 
 const App = () => {
-  return <div>App</div>;
+
+  const[courses,setCourses] = useState(null);
+  useEffect(() => {
+    const fetchData = async() => {
+    try{
+      const res = await fetch(apiUrl);
+      const output = await res.json();
+      setCourses(output.data);
+    }
+    catch(error){
+      toast.error("Something went wrong");
+    }
+  }
+  fetchData();
+  },[])
+
+
+  return (
+    <div>
+      <Navbar/>
+
+      <Filter
+        apiUrl={apiUrl}
+        filterData={filterData}
+      />
+
+      {/* <Cards/> */}
+    </div>
+  );
 };
 
 export default App;
